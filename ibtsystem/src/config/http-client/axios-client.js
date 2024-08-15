f6d8e68549dbe4c2ecd8e2eec587c50fc62073e7
Nieobe Hernandez <20223tn057@utez.edu.mx>
2024-08-15 15:29:58 -0600

    actualización de rutas

--- ibtsystem/src/config/http-client/axios-client.js
+++ ibtsystem/src/config/http-client/axios-client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const SERVER_URL = 'http://132.248.32.14'; // URL del servidor Spring Boot
+const SERVER_URL = 'http://132.248.32.14:8080'; // URL del servidor Spring Boot
 
 const AxiosClient = axios.create({
     baseURL: SERVER_URL,
